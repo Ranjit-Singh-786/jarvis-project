001ccc3b346faa6f814afeecebbdeb0021c2e9d5
Ranjit-Singh-786 <jiradhey402@gmail.com>
2021-06-19 17:22:47 +0530

    for M problem solve

--- jarvis.py
+++ jarvis.py
@@ -4,16 +4,16 @@ hour = datetime.datetime.now().hour
 minute = datetime.datetime.now().minute
 second = datetime.datetime.now().second
 print(corrent_time_and_date)
-# hour = int(input("enter your hour:- "))
-# if hour > 0 and hour < 12 :
-#     print("good morning")
-# elif hour >= 12 and hour <=18:
-#     print("good afternoon")
-# else:
-#     print("good evening")
-
-
-if __name__ == "__main__" :          #this is nothing this is true condition
-    print('jai mata di')
+hour = int(input("enter your hour:- "))
+if hour > 0 and hour < 12 :
+    print("good morning")
+elif hour >= 12 and hour <=18:
+    print("good afternoon")
 else:
-    print("not equal")
+    print("good evening")
+
+
+# if __name__ == "__main__" :          #this is nothing this is true condition
+#     print('jai mata di')
+# else:
+#     print("not equal")
